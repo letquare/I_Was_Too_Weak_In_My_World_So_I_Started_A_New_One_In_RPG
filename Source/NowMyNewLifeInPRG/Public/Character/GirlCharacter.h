e1d6e878f0c72ac6948e76b22b32d28280e59197
Ganeev Artem <68654620+letquare@users.noreply.github.com>
2026-06-02 19:36:32 +0300

    Added Enemy and GHero class

---
+++ Source/NowMyNewLifeInPRG/Public/Character/GirlCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/CharacterBase.h"
+#include "GirlCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOWMYNEWLIFEINPRG_API AGirlCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
