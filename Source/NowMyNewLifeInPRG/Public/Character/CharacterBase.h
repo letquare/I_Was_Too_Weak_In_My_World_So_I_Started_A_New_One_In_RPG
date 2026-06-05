ce299bf16e06a0d836515a3a9740a245fae09a41
Ganeev Artem <68654620+letquare@users.noreply.github.com>
2026-06-05 16:04:59 +0300

    Added Player Controller and IA_Move, IMP

---
+++ Source/NowMyNewLifeInPRG/Public/Character/CharacterBase.h
@@ -0,0 +1,23 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
+
+UCLASS(Abstract)
+class NOWMYNEWLIFEINPRG_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+	
+};
---
+++ Source/NowMyNewLifeInPRG/Public/Player/HeroPlayerController.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HeroPlayerController.generated.h"
+
+UCLASS()
+class NOWMYNEWLIFEINPRG_API AHeroPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
